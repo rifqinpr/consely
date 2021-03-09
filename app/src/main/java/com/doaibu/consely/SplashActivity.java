82b9f49d1a16ac2ace69133d176e5c32f99e5dec
MenaraWas <54022002+MenaraWas@users.noreply.github.com>
2021-03-09 08:05:09 +0700

    Splash Screen Update

---
+++ app/src/main/java/com/doaibu/consely/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.doaibu.consely;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        int SPLASH_TIME =3000;
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_TIME);
+    }
+}
